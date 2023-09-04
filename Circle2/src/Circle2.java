b82d8915c602c7c8e6a621e77939913e128297c3
syl-ches <138123177+syl-ches@users.noreply.github.com>
2023-09-04 18:02:08 +0800

    Add files via upload

---
+++ Circle2/src/Circle2.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+public class Circle2 {
+    public static void main (String[] args) {
+        double radius;
+        double area;
+        area = 100.0;
+        radius= Math.sqrt(area / Math.PI);
+        // Show data about circle
+        System.out.println();
+        System.out.println();
+        System.out.println(" ******************************************************** ");
+        System.out.println(" *    Given area of Circle = " + area +"                  ");
+        System.out.println(" *    Computed radius of circle = " + radius +"           ");
+        System.out.println(" ******************************************************** ");
+
+
+    }
+}
