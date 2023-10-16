98c57e838504fa0c3cc3422646c3270f12ef55be
Mark Chester P. Caliva <138123177+syl-ches@users.noreply.github.com>
2023-10-16 16:02:56 +0800

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,24 @@
+public class Arrays {
+    public static void main(String[] args) {
+       int [] numbers = {1,3,5,9};
+
+
+       for (int i = 0; i<4;i++) {
+
+           System.out.println(numbers[i]);
+
+       }
+        System.out.println(numbers.length);
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+
